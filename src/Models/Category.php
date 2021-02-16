70c6e907c3a4c71b732565f96824ddb6a6209273
Mihai <mihai.ocneanu@gmail.com>
2021-02-16 18:13:25 +0200

    added global scope & small changes

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Collection;
 use Illuminate\Support\LazyCollection;
+use LaravelEnso\Categories\Scopes\Ordered;
 use LaravelEnso\DynamicMethods\Traits\Abilities;
 use LaravelEnso\Helpers\Traits\AvoidsDeletionConflicts;
 use LaravelEnso\Tables\Traits\TableCache;
@@ -30,8 +31,7 @@ class Category extends Model
 
     public function subcategories(): Relation
     {
-        return $this->hasMany(static::class, 'parent_id')
-            ->orderBy('order_index');
+        return $this->hasMany(static::class, 'parent_id');
     }
 
     public function recursiveSubcategories(): Relation
@@ -50,7 +50,7 @@ class Category extends Model
         $oldParentId = $this->parent_id;
 
         $order = $orderIndex >= $this->order_index
-            && $oldParentId === $parentId
+        && $oldParentId === $parentId
             ? 'asc'
             : 'desc';
 
@@ -69,57 +69,46 @@ class Category extends Model
     public static function reorder(?int $parentId, string $order = 'asc')
     {
         self::whereParentId($parentId)
-            ->orderBy('order_index')
             ->orderBy('updated_at', $order)
             ->get()
-            ->each(fn ($group, $index) => $group
+            ->each(fn($group, $index) => $group
                 ->update(['order_index' => $index + 1]));
     }
 
     public static function tree(): Collection
     {
         return self::topLevel()
-            ->orderBy('order_index')
-            ->with(['recursiveSubcategories' => fn ($categories) => $categories
-                ->orderBy('order_index'), ])
+            ->with('recursiveSubcategories')
             ->get();
-    }
-
-    public function getParentTreeAttribute()
-    {
-        return $this->parentTree();
     }
 
     public function parentTree(): Collection
     {
         $category = $this;
-        $category->attributes['parent'] = $category->recursiveParent;
+
         $tree = Collection::wrap($category);
 
-        if ($category = $category->parent) {
+        while ($category = $category->recursiveParent) {
             $tree->prepend($category);
         }
 
         return $tree;
     }
 
-    public function parents(): LazyCollection
+    public static function filterTree()
     {
-        return LazyCollection::make(function () {
-            $parent = $this->parent;
+    }
 
-            while ($parent !== null) {
-                yield $parent;
-
-                $parent = $parent->parent;
-            }
-        });
+    public function flattenCurrentAndBelowIds(): Collection
+    {
+        return $this->flattenCurrentAndBelow()
+            ->pluck('id');
     }
 
     public function flattenCurrentAndBelow(): Collection
     {
         return $this->recursiveSubcategories
-            ->map(fn ($cat) => $cat->flattenCurrentAndBelow())
+            ->map(fn($cat) => $cat->flattenCurrentAndBelow())
             ->flatten()
             ->prepend($this);
     }
@@ -139,7 +128,12 @@ class Category extends Model
     public function depth(): int
     {
         return $this->recursiveSubcategories
-            ->map(fn ($category) => $category->depth() + 1)
-            ->max() ?? 0;
+                ->map(fn($category) => $category->depth() + 1)
+                ->max() ?? 0;
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new Ordered());
     }
 }
