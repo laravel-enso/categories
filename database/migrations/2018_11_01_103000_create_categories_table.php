0e9958e6eb25eb71f28f92e4bdd8fedf9b5d5806
mishu <mishu@xtelecom.ro>
2023-02-15 16:25:05 +0200

    adds name index

--- database/migrations/2018_11_01_103000_create_categories_table.php
+++ database/migrations/2018_11_01_103000_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->foreign('parent_id')->references('id')->on('categories')
                 ->onUpdate('restrict')->onDelete('restrict');
 
-            $table->string('name');
+            $table->string('name')->index();
 
             $table->unsignedInteger('order_index');
 
